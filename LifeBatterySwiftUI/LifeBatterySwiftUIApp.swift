bc41218f9b282fd2bb42fa04cb57798f7c43675c
Ru2Lu <jiutianlian0@gmail.com>
2023-08-28 20:10:19 +0900

    Initial Commit

---
+++ LifeBatterySwiftUI/LifeBatterySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeBatterySwiftUIApp.swift
+//  LifeBatterySwiftUI
+//
+//  Created by rururu on 2023/08/28.
+//
+
+import SwiftUI
+
+@main
+struct LifeBatterySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
